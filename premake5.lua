b51690a9887f6f89e67a0358ca3bb30d3bb114c0
YouMu <lewyoumu@gmail.com>
2025-04-09 21:45:40 +0800

    build: update premake file

--- premake5.lua
+++ premake5.lua
@@ -1,26 +1,36 @@
 project "ImGui"
 	kind "StaticLib"
 	language "C++"
-    staticruntime "off"
+	cppdialect "C++20"
+	staticruntime "On"
 
 	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
 	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
 
 	files
 	{
-		"imconfig.h",
-		"imgui.h",
-		"imgui.cpp",
-		"imgui_draw.cpp",
-		"imgui_internal.h",
-		"imgui_tables.cpp",
-		"imgui_widgets.cpp",
-		"imstb_rectpack.h",
-		"imstb_textedit.h",
-		"imstb_truetype.h",
-		"imgui_demo.cpp"
+		-- "imconfig.h",
+		-- "imgui.h",
+		-- "imgui.cpp",
+		-- "imgui_draw.cpp",
+		-- "imgui_internal.h",
+		-- "imgui_tables.cpp",
+		-- "imgui_widgets.cpp",
+		-- "imstb_rectpack.h",
+		-- "imstb_textedit.h",
+		-- "imstb_truetype.h",
+		-- "imgui_demo.cpp"
+		"*.h",
+		"*.cpp",
 	}
 
 	filter "system:windows"
 		systemversion "latest"
-		cppdialect "C++20"
+
+	filter "configurations:Debug"
+		symbols "On"
+		runtime "Debug"
+
+	filter "configurations:Release"
+		optimize "On"
+		runtime "Release"
